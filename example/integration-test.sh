e99211cb03f0709e222bd3ce0e0a948ca900f515
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-09-04 21:24:31 +0200

    Refactor the integration test and add untracked sample

--- example/integration-test.sh
+++ example/integration-test.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-count_file=$(mktemp)
-echo 0 > $count_file
+JSON_PLUGIN_NAME=json
+COUNT_FILE=$(mktemp)
+
+echo 0 > $COUNT_FILE
 function increment_error_count() {
-    local current_value=$(< $count_file)
-    echo $((current_value + 1)) > $count_file
+    local current_value=$(< $COUNT_FILE)
+    echo $((current_value + 1)) > $COUNT_FILE
 }
 function s() {
-    [ $1 -le 1 ]|| echo s
+    [ "$1" -le 1 ]|| echo s
 }
 
 function lint_file() {
-    local file=$1
+    local file="$1"
     if [ ! -f "$file" ]; then return 1; fi
     npm run lint -- $file 2> /dev/null
 }
@@ -28,7 +30,7 @@ function check() {
     fi
 }
 function check_file() {
-    local file="samples/$1.json" type="$2" error_name="$3" count="$4"
+    local file="samples/$1.json" type="$2" error_name="$JSON_PLUGIN_NAME/$3" count="$4"
     if [ ! -f "$file" ]; then
         echo "Unexisting file $1 ($file)"
         increment_error_count
@@ -37,17 +39,16 @@ function check_file() {
     lint_file "$file" | check "$type" "$error_name" "$count" "'$file'"
 }
 
-json=json
-check_file good-json warning "$json/" 0
-check_file good-json warning "$json/" 0
-check_file duplicate-keys error "$json/duplicate-key" 2
-check_file wrong-syntax warning "$json/*" 1
-check_file whole-mess error "$json/duplicate-key" 2
-check_file whole-mess error "$json/trailing-comma" 1
-check_file whole-mess warning "$json/*" 1 # as comment-not-permitted
-check_file jsonc "*" "$json/json" 0 # comment allowed
+check_file good-json warning "$ANY" 0
+check_file good-json warning "$ANY" 0
+check_file duplicate-keys error "duplicate-key" 2
+check_file wrong-syntax warning "*" 1
+check_file whole-mess error "duplicate-key" 2
+check_file whole-mess error "trailing-comma" 1
+check_file whole-mess warning "*" 1 # as comment-not-permitted
+check_file jsonc "*" "json" 0 # comment allowed
 
-error_count=$(< $count_file)
+error_count=$(< $COUNT_FILE)
 echo
 if [ "$error_count" -gt 0 ]; then
     echo "Integration test, in total $error_count error$(s $error_count) occured"
