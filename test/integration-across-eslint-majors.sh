ac1f44e72ea9c5db6b5ecae58ed49d313a811110
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-05-13 00:30:14 +0200

    Rework travis configuration to ensure eslint majors supported :woman_scientist:

---
+++ test/integration-across-eslint-majors.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+cmd="$1"
+shift
+
+case "$cmd" in
+  test|install);;
+  *)
+    echo "Unknown integration subcommand $cmd"
+    exit 2
+  ;;
+esac
+
+if [[ -z "$@" ]]; then
+  echo "No eslint major versions were provided"
+  exit 2
+fi
+
+echo "Will perform $cmd for following eslint majors lines: $@"
+
+for version in $@; do
+    (cd test/packages/eslint-v$version && npm $cmd)
+done
